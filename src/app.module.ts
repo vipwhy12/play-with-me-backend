e3ce295bd9b74a74eb40a6c36cfacd4822d270e2
Yuna <85014628+vipwhy12@users.noreply.github.com>
2023-12-25 05:52:46 +0900

    feat: connecconnect database🗃️

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.development.env',
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DATABASE_HOST,
+      port: parseInt(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: 'playwithme',
+      entities: [__dirname + '/**/*/.entity{.ts,.js}'],
+      synchronize: true, //🚨데이터 베이스 초기화로 인해 프로덕션시 제거
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
