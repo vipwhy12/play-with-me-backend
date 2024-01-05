eb2a93755d13938490a2a41d676c964eef55609c
Yuna <85014628+vipwhy12@users.noreply.github.com>
2024-01-06 01:03:23 +0900

    feat : ➕ nestjs/passport install

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 import { ChatsModule } from './chats/chats.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -23,6 +24,7 @@ import { ChatsModule } from './chats/chats.module';
     }),
     UsersModule,
     ChatsModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
