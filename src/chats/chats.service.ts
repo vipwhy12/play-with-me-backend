3ac585efc3b9c1a717cefba2bcb0d0271c3cc8d2
vipwhy12 <vipwhy12@naver.com>
2024-02-02 16:33:08 +0900

    feat: 채팅방 목록, 페이징

--- src/chats/chats.service.ts
+++ src/chats/chats.service.ts
@@ -3,6 +3,7 @@ import {
   HttpException,
   HttpStatus,
   Injectable,
+  NotFoundException,
 } from '@nestjs/common';
 import { ChatsRepository } from './chats.repository';
 import { ChatRegisterDto } from './dto/chat-register.dto';
@@ -14,7 +15,7 @@ export class ChatsService {
   constructor(private chatsRepository: ChatsRepository) {}
 
   async registerChat(chatRegisterDto: ChatRegisterDto): Promise<ChatsModel> {
-    const newChat = this.chatsRepository.register(chatRegisterDto);
+    const newChat = await this.chatsRepository.register(chatRegisterDto);
 
     if (!newChat) {
       throw new HttpException(
@@ -34,5 +35,33 @@ export class ChatsService {
     }
 
     return await this.chatsRepository.deleteChat(id);
+  }
+
+  async getAllChat(page: number): Promise<any> {
+    const take = 10; //가져올 채팅방 데이터의 개수
+    const numberOfChat = await this.chatsRepository.countChats();
+
+    if (numberOfChat === 0) {
+      return new HttpException('채팅이 없습니다.', HttpStatus.NO_CONTENT);
+    }
+
+    const lastPage = Math.ceil(numberOfChat / take);
+
+    if (!page) {
+      page = 1;
+    }
+
+    if (page < 1 || page > lastPage) {
+      throw new NotFoundException('🧨 해당 페이지는 존재하지 않습니다');
+    }
+
+    const getChatList = await this.chatsRepository.getAllChat(take, page);
+
+    return {
+      numberOfChat,
+      presentPage: page,
+      lastPage,
+      getChatList,
+    };
   }
 }
